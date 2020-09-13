cd614a230c36b7e2acfa91829032c1eda32d50b9
tangjx <tyuttjx@163.com>
2020-09-13 18:55:54 +0800

    feat: 冒泡排序

---
+++ src/main/java/cn/nowdo/algorithm/sort/bubble/BubbleSortDemo.java
@@ -0,0 +1,34 @@
+package cn.nowdo.algorithm.sort.bubble;
+
+import java.util.Scanner;
+
+/**
+ *
+ */
+public class BubbleSortDemo {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        //输入数据组长度
+        System.out.println("input array length ...");
+        int length = scanner.nextInt();
+        //创建并初始化数据
+        int[] array = new int[length];
+        System.out.println("input arrays ...");
+        for (int i = 0; i < array.length; i ++) {
+            array[i] = scanner.nextInt();
+        }
+        for (int i = 0; i < array.length -1; i ++) {
+            for (int j = 0; j < array.length -1 - i; j ++) {
+                if(array[j] > array[j + 1]) {
+                    int tmp = array[j];
+                    array[j] = array[j + 1];
+                    array[j + 1] = tmp;
+                }
+            }
+        }
+        System.out.println("sort complete:");
+        for(int i = 0; i < array.length; i++) {
+            System.out.printf("%d ", array[i]);
+        }
+    }
+}
