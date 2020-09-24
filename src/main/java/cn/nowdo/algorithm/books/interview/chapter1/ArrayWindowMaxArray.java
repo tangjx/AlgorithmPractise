adb1c33624383d77fd1e400bc3574e6282ea67be
tangjx <tyuttjx@163.com>
2020-09-24 19:00:55 +0800

    feat: array window max array

--- src/main/java/cn/nowdo/algorithm/books/interview/chapter1/ArrayWindowMaxArray.java
+++ src/main/java/cn/nowdo/algorithm/books/interview/chapter1/ArrayWindowMaxArray.java
@@ -5,4 +5,26 @@ package cn.nowdo.algorithm.books.interview.chapter1;
  * @Date 2020/9/24 17:59
  */
 public class ArrayWindowMaxArray {
+    public static Integer[] getArrayWindowMaxArray(Integer[] array, int window) {
+        Integer[] windowMaxArray = new Integer[array.length - window + 1];
+        for(int i = 0; i < windowMaxArray.length; i ++) {
+            Integer max = 0;
+            for(int j = i; j < i + 3; j ++) {//遍历窗口数组
+                if(max < array[j]) {
+                    max = array[j];
+                }
+            }
+            //每个窗口数组的最大值
+            windowMaxArray[i] = max;
+        }
+        return windowMaxArray;
+    }
+    public static void main(String[] args) {
+        Integer[] array = new Integer[] {4,3,5,4,3,3,6,7};
+        int window = 3;
+        Integer[] windowMaxArray = getArrayWindowMaxArray(array, window);
+        for(int i = 0; i < array.length - window +1; i ++) {
+            System.out.println(windowMaxArray[i]);
+        }
+    }
 }
